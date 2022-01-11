df2c5c8c3134a5b558a7f1fae739165ebf0bd9ee
DiegoCardosoDev <diegocardososacramento@gmail.com>
2022-01-11 19:15:43 -0300

    project init

---
+++ backend/springreact/src/test/java/com/diego/springreact/SpringreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.springreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringreactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
