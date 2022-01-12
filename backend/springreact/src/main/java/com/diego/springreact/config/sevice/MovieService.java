08c90796f30d105dc1e3cfc54a07b9552725097f
DiegoCardosoDev <diegocardososacramento@gmail.com>
2022-01-12 09:18:41 -0300

    Find movies

--- backend/springreact/src/main/java/com/diego/springreact/config/sevice/MovieService.java
+++ backend/springreact/src/main/java/com/diego/springreact/config/sevice/MovieService.java
@@ -32,4 +32,16 @@ public class MovieService {
     }
 
 
+
+    //retorna um filme por seu id
+    @Transactional(readOnly = true)
+    public MovieDTO findById(Long id){
+
+        Movie result =  repository.findById(id).get();
+        MovieDTO dto = new MovieDTO(result);
+        return dto;
+
+    }
+
+
 }
--- backend/springreact/src/main/java/com/diego/springreact/controller/MovieController.java
+++ backend/springreact/src/main/java/com/diego/springreact/controller/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,11 @@ public class MovieController {
         return service.findAll(pageable);
 
     }
+
+    @GetMapping(value = "/{id}")
+    public MovieDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+
 }
