81abefd48d0c1fb480f102ea8b521580ed42accf
DiegoCardosoDev <diegocardososacramento@gmail.com>
2022-01-15 00:28:09 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,20 +1,40 @@
+
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "ultils/request";
+import {MoviePage} from "types/movie";
 
 function Listing(){
 
-    //forama errdada
-    axios.get(`${BASE_URL} /movies?size=12&page=0`)
 
-    .then(response => {
-        console.log(response.data);
-    });
+    const [pageNUmber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+
+        axios.get(`${BASE_URL} /movies?size=12&page=1`)
+
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+    
+            setPageNumber(data.number);
+    
+    
+        });
+
+        
+
+    }, []);
+
 
     return(
 
+
        <>
+               <p>{pageNUmber}</p>
+
         <Pagination/>
 
         <div className="container">
